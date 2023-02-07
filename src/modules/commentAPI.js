b75be5eba4f80444d1eb556c05673f54cee9d056
biruk-bereka <birukbeletech1212@gmail.com>
2023-02-07 19:04:53 +0300

    Add involvement api for comments

---
+++ src/modules/commentAPI.js
@@ -0,0 +1,28 @@
+export default class CommentAPI {
+    constructor() {
+        this.baseURL= `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/WWOK0bWjrP9cmMnhRfLg`
+    }
+
+    addComment = async (id,name,comment) => {
+       await fetch(`${this.baseURL}/comments`, {
+          method: 'POST',
+          body: JSON.stringify({
+              item_id: id,
+              username: name,
+              comment: comment
+          }),
+          headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+          },
+       })
+       .then((response) => response.json());
+    }
+
+    getComments = async (id) => {
+        const comments = await fetch(`${this.baseURL}/comments?item_id=${id}`)
+        .then(response => response.json())
+    
+        if(comments) return comments;
+        return [];
+    }
+}
