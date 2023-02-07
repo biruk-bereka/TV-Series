74a50b7d02b86f7343a3f4eb0a725afbc3d8e7d5
biruk-bereka <birukbeletech1212@gmail.com>
2023-02-07 11:39:22 +0300

    style comment popup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import comment from './modules/comment';
 import './css/style.css';
+
+const commentBtn = document.querySelectorAll('.comment');
+
+commentBtn.forEach((btn,index) => {
+  btn.addEventListener('click', ()=>{
+     comment(index);
+  })
+})
---
+++ src/modules/comment.js
@@ -0,0 +1,13 @@
+
+const comment = (index) => {
+  
+    // const commentPopup = document.createElement('section');
+    // commentPopup.classList.add('popup');
+    // commentPopup.innerHTML = `
+    //    <p>Comment</p>
+    // `
+
+}
+
+
+export default comment; 
