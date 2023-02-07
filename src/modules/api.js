cc42b25d4480e50361a6fc842f64be19b74e9a35
Kender ROMAIN <kenderromain@gmail.com>
2023-02-06 22:04:44 -0400

    Add a loader for the list items.

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,7 +1,4 @@
 const baseURL = 'https://api.tvmaze.com/shows?page=1';
-const baseURLInvolvement =
-  'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/ER2rFBidmmnXOpDOOVHm';
-
 export default class Api {
   static async getFilms() {
     const req = await fetch(baseURL);
--- src/modules/ui.js
+++ src/modules/ui.js
@@ -4,25 +4,28 @@ const listItems = document.querySelector('#list-items');
 const homeLink = document.querySelector('#home');
 
 const displayFilms = async () => {
+  const loader = document.querySelector('#loading');
+  loader.classList.add('display');
   // Get the series from the API
   const data = await Api.getFilms();
+  loader.classList.remove('display');
   // Count the series
   const filmsCount = data.length;
   // Update the home link accordingly
   homeLink.innerHTML = `TV Series (${filmsCount})`;
-  let items = ``;
-  data.forEach((film, index) => {
+  let items = '';
+  data.forEach((film) => {
     items += `
     <article class="item">
       <img src="${film.image.original}" alt="">
           <div class="title">
           <span class="film">${film.name}</span>
-          <button class="like-btn">
+          <button class="like-btn" type="button">
           <span data-id="${film.id}" class="material-symbols-outlined">favorite</span>
           </button>
           <span class="like"> 5 likes</span>
           </div>
-          <button data-id="${film.id}" class="comment-btn">Comment</button>
+          <button data-id="${film.id}" class="comment-btn" type="button">Comment</button>
     </article>
     `;
     listItems.innerHTML = items;
